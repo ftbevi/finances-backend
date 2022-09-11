c1b9b7d71acec7e56de563a5e80e58af37342b4d
Thiago Beviláqua <thiago.bevilaqua@grupomateus.com>
2022-09-11 14:44:28 -0300

    add models and data definitions for transactions

---
+++ finances/wallets/models.py
@@ -0,0 +1,54 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+
+from finances.accounts.models import BaseModel, User
+
+
+class TransactionType:
+    INVOICE = "invoice"
+    REVENUE = "revenue"
+
+    TRANSACTION_CHOICES = [
+        (INVOICE, "invoice"),
+        (REVENUE, "revenue"),
+    ]
+
+
+class Category(BaseModel):
+    title = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+
+
+class Transaction(BaseModel):
+    owner = models.ForeignKey(User, verbose_name="owner", on_delete=models.PROTECT)
+    title = models.TextField()
+    transaction_type = models.CharField(
+        verbose_name="type",
+        max_length=12,
+        choices=TransactionType.TRANSACTION_CHOICES,
+        default=TransactionType.INVOICE,
+    )
+    category = models.ForeignKey(
+        "Category",
+        verbose_name="category",
+        on_delete=models.PROTECT,
+        related_name="transaction",
+    )
+    amount = models.DecimalField(verbose_name="amount", max_digits=11, decimal_places=2)
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+
+    def save(self, *args, **kwargs):
+        Transaction.validate_negative_amount(self.amount)
+        super().save(*args, **kwargs)
+
+    @classmethod
+    def validate_negative_amount(self, amount):
+        if amount < 0:
+            raise ValidationError(f"{self.amount} is not an monetary valid number")
