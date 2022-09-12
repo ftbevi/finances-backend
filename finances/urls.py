f59403f1fed5744c3dc76429907771263e8b97c2
Thiago Beviláqua <thiago.bevilaqua@grupomateus.com>
2022-09-12 04:36:58 -0300

    change admin page for general route.

--- finances/urls.py
+++ finances/urls.py
@@ -21,7 +21,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("", admin.site.urls),
     path("api/transactions/", include(wallet_routers)),
     re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
